c206ac8c1c2db9baaf686083ead1dd858723539c
Augusto-LJ <augustolimajardim@gmail.com>
2025-09-30 17:57:10 -0300

    feat: simple input with alphabet below

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
 
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 </script>
 
 <template>
-TESTE HOME
+    <div class="flex flex-col p-8">
+        <input  type="text"
+                class="rounded border-2 border-gray-200 w-full"
+                placeholder="Search for meals"
+        />
+
+        <div class="flex justify-center gap-2 mt-2">
+            <router-link to="/" v-for="letter in letters">
+                {{ letter }}
+            </router-link>
+        </div>
+    </div>
 </template>
