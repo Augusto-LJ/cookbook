1291a87f7656f0aa656517970b8ab744eb34cb83
Augusto-LJ <augustolimajardim@gmail.com>
2025-10-17 17:01:58 -0300

    feat: reorganizes routes with new components

---
+++ src/components/DefaultLayout.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import NavBar from "./NavBar.vue";
+</script>
+
+<template>
+    <div class="bg-gray-100 h-full">
+        <NavBar />
+        <main>
+            <router-view />
+        </main>
+    </div>
+</template>
+
