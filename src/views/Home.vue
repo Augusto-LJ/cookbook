3a9f2bca26e07b128533665f2484507aaa42f1ab
Augusto-LJ <augustolimajardim@gmail.com>
2025-11-15 18:21:03 -0300

    feat: updates home page with random meals

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 import axiosClient from '../axiosClient';
+import Meals from '../components/Meals.vue';
 
-const ingredients = ref([]);
+const meals = ref<any>([]);
 
 onMounted(async () => {
+    for (let i = 0; i < 9; i++){
+        axiosClient.get(`random.php`).then(({data}) => meals.value.push(data.meals[0]));
+    }
     const response = await axiosClient.get('/list.php?i=list');
-    ingredients.value = response.data;
+    meals.value = response.data;
 })
 </script>
 
 <template>
-    <div class="flex flex-col p-8">
-        
+    <div class="p-8 pb-0 text-orange-500">
+        <h1 class="text-4xl font-bold mb-4">Random meals</h1>
     </div>
+    <Meals :meals="meals"/>
 </template>
